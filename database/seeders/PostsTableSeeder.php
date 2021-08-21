d593d723134c2738f294c77f0e27aa4673b5a42b
Shengyou Fan <shengyoufan@gmail.com>
2021-08-21 18:43:41 +0800

    把 Seeder 變得更好用

--- database/seeders/PostsTableSeeder.php
+++ database/seeders/PostsTableSeeder.php
@@ -3,7 +3,9 @@
 namespace Database\Seeders;
 
 use App\Models\Post;
+use Faker\Factory;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Carbon;
 
 class PostsTableSeeder extends Seeder
 {
@@ -14,10 +16,17 @@ class PostsTableSeeder extends Seeder
      */
     public function run()
     {
-        foreach (range(1, 10) as $id) {
+        Post::truncate();
+
+        $faker = Factory::create('zh_TW');
+        $amount = 10;
+
+        foreach (range(1, $amount) as $id) {
             Post::create([
-                'title' => "Title $id",
-                'content' => "Content $id",
+                'title' => $faker->sentence,
+                'content' => $faker->paragraph,
+                'created_at' => Carbon::now()->subDays($amount - $id),
+                'updated_at' => Carbon::now()->subDays($amount - $id)->addHours(rand(1, 12))->addMinutes(rand(1, 60)),
             ]);
         }
     }
