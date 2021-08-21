839704d547a99a54ca3ef48db741214d2b44f06d
Shengyou Fan <shengyoufan@gmail.com>
2021-08-21 18:03:57 +0800

    匯入樣板

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [HomeController::class, 'index'])->name("home.index");
+Route::get('posts', [PostController::class, 'index'])->name("posts.index");
+Route::get('posts/{id}', [PostController::class, 'show'])->name("posts.show");
