20e2ba1f0347dfa124e68e6112472a8c7d34025a
Shengyou Fan <shengyoufan@gmail.com>
2021-08-21 18:48:11 +0800

    改用 Model Factory 做 Seeding

--- database/seeders/PostsTableSeeder.php
+++ database/seeders/PostsTableSeeder.php
@@ -17,17 +17,6 @@ class PostsTableSeeder extends Seeder
     public function run()
     {
         Post::truncate();
-
-        $faker = Factory::create('zh_TW');
-        $amount = 10;
-
-        foreach (range(1, $amount) as $id) {
-            Post::create([
-                'title' => $faker->sentence,
-                'content' => $faker->paragraph,
-                'created_at' => Carbon::now()->subDays($amount - $id),
-                'updated_at' => Carbon::now()->subDays($amount - $id)->addHours(rand(1, 12))->addMinutes(rand(1, 60)),
-            ]);
-        }
+        Post::factory(15)->create();
     }
 }
